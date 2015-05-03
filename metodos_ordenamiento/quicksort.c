e0eb42a026206e3f2559c0471578e28a50d77ef0
c4explosive <ao946391@gmail.com>
2015-05-03 12:20:35 -0500

    QuickSort Recursive

--- metodos_ordenamiento/quicksort.c
+++ metodos_ordenamiento/quicksort.c
@@ -3,36 +3,74 @@
 //#include <conio.h>
 #define clrscr() system("clear")
 #define getch() system("read")
-#define TAM 0x06
+#define TAM 0x0A
 //Limites o rango
-#define LIMS 0x0A
+#define LIMS 0x4A
 #define LIMF 0x01
 //Metodos para ordenar//////////
-///////////Quicksort////////////////
-int * quicksort(int * array, int o);
-/////////////////////////////////////
+///////////Quicksort///////////////////////////////////
+void quicksort(int * array,int prin, int fin,  int o);
+int particionar(int * array,int ipiv,int prin, int fini,int o);
+void swap(int* array, int num1, int num2);
+///////////////////////////////////////////////////////
 int * gen(int * array);
 void print();
 void main()
 {
     int * array=malloc(TAM*sizeof(int));
-    clrscr();
+    srand(time(NULL));
+    clrscr(); 
     array=gen(array); 
     print(array);
-    array=quicksort(array,0); //1 when is normal and 0 when is reverse
+    quicksort(array,0,TAM-1,1); //1 when is normal and 0 when is reverse
+    print(array);
+    array=gen(array); 
+    print(array);
+    quicksort(array,0,TAM-1,1); //1 when is normal and 0 when is reverse
+    print(array);
     getch();
 
 }
-int * quicksort(int * array, int o)
+void quicksort(int * array,int prin, int fin,  int o)
 {
-    //int ipivot=
-    
+    int ipiv=fin,pix;
+    if (prin<fin)
+    {
+    	pix=particionar(array,ipiv,prin,fin,o);
+	quicksort(array,prin,pix-1,o);
+	quicksort(array,pix+1,fin,o);
+    }
+}
+int particionar(int * array,int ipiv,int prin, int fin,int o)
+{
+    int i,pix,mode;
+    pix=prin;
+    for (i=prin;i<fin;i++)
+    {
+	if (o)
+	    mode=array[i] <= array[ipiv];
+	else
+	    mode=array[i] >= array[ipiv];	
+	if (mode)
+	{
+	     swap(array,i,pix);
+	     pix++;
+	}
+    }
+    swap(array,pix,fin);
+    return pix;
+}
+void swap(int *array, int num1, int num2)
+{
+    int aux;
+    aux=array[num1];
+    array[num1]=array[num2];
+    array[num2]=aux;
 }
 //Generador de Array
 int * gen(int * array)
 {
     int i,j,val,igual;
-    srand(time(NULL));
     for(i=0;i<TAM;i++)
     {
     	 array[i]=LIMF+rand()%((LIMS+1)-LIMF);
